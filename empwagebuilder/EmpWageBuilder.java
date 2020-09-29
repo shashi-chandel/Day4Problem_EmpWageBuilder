993b2614110d7cf8aea5d079b856693feeda9500
Your Name <you@example.com>
2020-09-29 15:20:27 +0530

    [shashi-chandel] add . Employee Part Time

--- empwagebuilder/EmpWageBuilder.java
+++ empwagebuilder/EmpWageBuilder.java
@@ -1,14 +1,17 @@
 package com.empwagebuilder;
 
 public class EmpWageBuilder {
-		private static final int IS_FULL_TIME = 1;
+		private static final int IS_PART_TIME = 1;
+		private static final int IS_FULL_TIME = 2;
 		private static final int EMP_RATE_PER_HOUR = 20;
 		
 		public static void main(String[] args) {
 			int empHrs = 0;
 			int empWage = 0;
 			double empCheck = Math.floor(Math.random() * 10) % 2;
-			if (empCheck == IS_FULL_TIME)
+			if (empCheck == IS_PART_TIME)
+				empHrs = 4;
+			else if (empCheck == IS_FULL_TIME)
 				empHrs = 8;
 			else
 				empHrs = 0;
