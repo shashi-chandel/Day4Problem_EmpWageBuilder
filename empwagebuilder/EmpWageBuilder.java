8727d437bbf8cabd81b5225d303817cf5b676c01
Your Name <you@example.com>
2020-09-29 15:27:39 +0530

    [shashi-chandel] add . Employee Monthly Wage Calculated

--- empwagebuilder/EmpWageBuilder.java
+++ empwagebuilder/EmpWageBuilder.java
@@ -4,10 +4,10 @@ public class EmpWageBuilder {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
-	
+	public static final int NUM_OF_WORKING_DAYS = 20;
 	public static void main(String[] args) {
-		int empHrs = 0;
-		int empWage = 0;
+		int empHrs = 0, empWage = 0, totalEmpWage = 0;
+		for (int day=0; day< NUM_OF_WORKING_DAYS;day++) {
 		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 		switch (empCheck) 
 		{
@@ -21,6 +21,9 @@ public class EmpWageBuilder {
 			empHrs = 0;
 		}
 		empWage = empHrs * EMP_RATE_PER_HOUR;
+		totalEmpWage += empWage;
 		System.out.println("Emp Wage: " + empWage);
+	}
+		System.out.println("Total Emp Wage: " + totalEmpWage);
 	}
 }
