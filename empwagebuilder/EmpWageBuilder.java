e656b4bac32eaf78991e0c62370a1ce5e8afb8d0
Your Name <you@example.com>
2020-09-29 16:11:24 +0530

    [shashi-chandel] add . Employee Wage using Procedural Way

--- empwagebuilder/EmpWageBuilder.java
+++ empwagebuilder/EmpWageBuilder.java
@@ -3,34 +3,32 @@ package com.empwagebuilder;
 public class EmpWageBuilder {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
-	public static final int EMP_RATE_PER_HOUR = 20;
-	public static final int NUM_OF_WORKING_DAYS = 20;
-	public static final int MAX_HRS_IN_MONTH = 100;
 	
-	public static int computeEmpWage() {
+	public static int computeEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
 		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-		while (totalEmpHrs  <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
-		totalWorkingDays++;
-		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-		switch (empCheck) 
-		{
-		case IS_PART_TIME:
-			empHrs = 4;
-			break;
-		case IS_FULL_TIME:
-			empHrs = 8;
-			break;
-		default:
-			empHrs = 0;
-		}
-		totalEmpHrs += empHrs;
-		System.out.println("Day: " + totalWorkingDays + " Emp Hr: " +empHrs);
-		}
-		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Total Emp Wage: " + totalEmpWage);
+		while (totalEmpHrs  <= maxHoursPerMonth  && totalWorkingDays < numOfWorkingDays) {
+				totalWorkingDays++;
+				int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+				switch (empCheck) 
+				{
+				case IS_PART_TIME:
+					empHrs = 4;
+					break;
+				case IS_FULL_TIME:
+					empHrs = 8;
+					break;
+				default:
+					empHrs = 0;
+				}
+			totalEmpHrs += empHrs;
+			System.out.println("Day: " + totalWorkingDays + " Employee Hours: " +empHrs);
+			}
+		int totalEmpWage = totalEmpHrs * empRatePerHour;
+		System.out.println("Total Emp Wage for Company '" +company+ "' is : " + totalEmpWage);
 		return totalEmpWage;
 	}
 	public static void main(String[] args) {
-		computeEmpWage();
+		computeEmpWage("Dmart", 20, 2, 10);
+		computeEmpWage("Jio", 15, 4, 20);
 	}
 }
