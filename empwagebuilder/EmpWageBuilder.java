e1b58dd32a53f1895c5aa42792762ff39ab95d9c
Your Name <you@example.com>
2020-09-29 15:04:36 +0530

    [shashi-chandel] add . Present Or Absent

--- empwagebuilder/EmpWageBuilder.java
+++ empwagebuilder/EmpWageBuilder.java
@@ -1,7 +1,14 @@
 package com.empwagebuilder;
 
 public class EmpWageBuilder {
+		private static final int IS_FULL_TIME = 1;
+	
 		public static void main(String[] args) {
-			System.out.println("Welcome to Employee Wage Computation!!!");
+			System.out.println("Welcome to Employee Wage Computation!!! \n");	
+			double empCheck = Math.floor(Math.random() * 10) % 2;
+			if (empCheck == IS_FULL_TIME)
+				System.out.println("Employee is Present");
+			else
+				System.out.println("Employee is Absent");
 		}
 }
