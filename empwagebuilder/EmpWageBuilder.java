c244142c11278e3721a166f4b3602bdf88cd8cac
Your Name <you@example.com>
2020-09-29 15:16:04 +0530

    [shashi-chandel] add . Employee Daily Wage

--- empwagebuilder/EmpWageBuilder.java
+++ empwagebuilder/EmpWageBuilder.java
@@ -2,13 +2,17 @@ package com.empwagebuilder;
 
 public class EmpWageBuilder {
 		private static final int IS_FULL_TIME = 1;
-	
+		private static final int EMP_RATE_PER_HOUR = 20;
+		
 		public static void main(String[] args) {
-			System.out.println("Welcome to Employee Wage Computation!!! \n");	
+			int empHrs = 0;
+			int empWage = 0;
 			double empCheck = Math.floor(Math.random() * 10) % 2;
 			if (empCheck == IS_FULL_TIME)
-				System.out.println("Employee is Present");
+				empHrs = 8;
 			else
-				System.out.println("Employee is Absent");
+				empHrs = 0;
+			empWage = empHrs * EMP_RATE_PER_HOUR;
+			System.out.println("Emp Wage: " + empWage);
 		}
 }
